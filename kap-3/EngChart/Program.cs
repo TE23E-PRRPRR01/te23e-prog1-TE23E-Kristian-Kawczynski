390e4127b0770e0027a39337fd45d89d194df81c
Kristian <kristian.kawczynski@elev.ga.ntig.se>
2024-09-04 15:01:47 +0200

    jag har jobbat med if statements

---
+++ kap-3/EngChart/Program.cs
@@ -0,0 +1,59 @@
+﻿Console.Clear();
+Console.WriteLine("Välkommen till ingenjörs flödeschema");
+
+Console.WriteLine(
+$"""
+                                       $"   *.      
+              d$$$$$$$P"                  $    J
+                  ^$.                     4r  "
+                  d"b                    .db
+                 P   $                  e" $
+        ..ec.. ."     *.              zP   $.zec..
+    .^        3*b.     *.           .P" .@"4F      "4
+  ."         d"  ^b.    *c        .$"  d"   $         %
+ /          P      $.    "c      d"   @     3r         3
+4        .eE........$r===e$$$$eeP    J       *..        b
+$       $$$$$       $   4$$$$$$$     F       d$$$.      4
+$       $$$$$       $   4$$$$$$$     L       *$$$"      4
+4         "      ""3P ===$$$$$$"     3                  P
+ *                 $       "'"        b                J
+  ".             .P                    %.             @
+    %.         z*"                      ^%.        .r"
+       "*==*""                             ^"*==*""  
+"""
+);
+
+
+Console.WriteLine("Does it move? (y/n) ");
+string answer = Console.ReadLine();
+
+if (answer == "y")
+{
+    Console.WriteLine("Should it? (y/n)");
+    answer = Console.ReadLine();
+    if (answer == "y")
+    {
+        Console.WriteLine("No problem!");
+        Console.ReadLine();
+    }
+    else
+    {
+        Console.WriteLine("Plz use Duct tape!");
+        Console.ReadLine();
+    }
+}
+else
+{
+    Console.WriteLine("Should it? (y/n)");
+    answer = Console.ReadLine();
+    if (answer == "y")
+    {
+        Console.WriteLine("Plz use WD-40");
+        Console.ReadLine();
+    }
+    else
+    {
+        Console.WriteLine("No problem!");
+        Console.ReadLine();
+    }
+}
---
+++ kap-3/InteractiveStory/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+Console.WriteLine("Välkommen till" );
+
+Console.WriteLine(
+$"""
+                        ____________          ____     ____________    
+                        \           \     ____\_  \__  \           \   
+                         \           \   /     /     \  \           \  
+                          |    /\     | /     /\      |  |    /\     | 
+                          |   |  |    ||     |  |     |  |   |  |    | 
+                          |    \/     ||     |  |     |  |    \/     | 
+                          /           /||     | /     /| /           /| 
+                         /___________/ ||\     \_____/ |/___________/ | 
+                         |           | / | \_____\   | /|           | /  
+                         |___________|/   \ |    |___|/ |___________|/   
+                                           \|____| 
+"""                    
+);
+
+Console.WriteLine("Dungeon of Doom!!!");
+
+Console.WriteLine("Du vaknar i ett rum där en fackla lyser upp den, vill du ta upp facklan (y/n)");
+string answer = Console.ReadLine();
+
+if (answer == "y")
+{
+    Console.WriteLine("Du ställer dig upp och tar up facklan!");
+    Console.ReadLine();
+}
+else
+{
+    Console.WriteLine("Facklan sloknar och du fastnar i rummet i mörkret");
+    Console.ReadLine();
+}
