964411bdae9e9f2d21af8ffef251d0b2c864c257
Kristian <kristian.kawczynski@elev.ga.ntig.se>
2024-11-20 15:01:24 +0100

    till punkt 5

---
+++ kap4/ForloopBonanza/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.Clear();
+//Console.WriteLine("Program som skriver ut heltail");
+
+//Startvärde
+//Console.Write("Ange ett startvärde: ");
+//int startvärde = int.Parse(Console.ReadLine()!);
+
+//Slutvärde
+//Console.Write("Ange ett slutvärde: ");
+//int slutvärde = int.Parse(Console.ReadLine()!);
+
+//int summa = 0;
+//Skriv ut
+//for (int i = startvärde; i < slutvärde+1; i++)
+{
+   // Console.WriteLine(i);
+    //summa += i;
+}
+    //Console.WriteLine($"Summan av alla heltalen är {summa}");
+
+
+//Startvärde
+Console.Write("Ange ett startvärde: ");
+int startvärde = int.Parse(Console.ReadLine()!);
+
+//Slutvärde
+Console.Write("Ange ett slutvärde: ");
+int slutvärde = int.Parse(Console.ReadLine()!);
+
+Console.Write("Jämna tal: ");
+for (int i = 1; i < 21; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write($"{i},");
+    }
+}
+Console.WriteLine();
+
+Console.Write("Udda tal: ");
+for (int i = 1; i < 21; i++)
+{
+    if (i % 2 != 0)
+    {
+        Console.Write($"{i}, ");
+    }
+}
