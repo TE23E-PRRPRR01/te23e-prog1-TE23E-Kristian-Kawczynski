b083fbf9d1adcfdf83edafbd13f97c6c3bb9a1d0
Kristian <kristian.kawczynski@elev.ga.ntig.se>
2024-12-02 14:51:53 +0100

    jobbat

---
+++ kap5/Mikroövningar 1/Program.cs
@@ -0,0 +1,29 @@
+﻿//Lista program
+List<string> listaNamn = ["Miruko", "Monika"];
+
+string.Join(", ", listaNamn);
+
+Console.WriteLine($"abc: {string.Join(", ", listaNamn)}");
+
+// foreach (var namn in listaNamn)
+// {
+//     Console.Write($"{namn}, ");
+// }
+
+//finns i listan
+// if (listaNamn.Contains(""))
+// {
+//     Console.WriteLine("Den finns");
+// }
+// else
+// {
+//     Console.WriteLine("Den finns inte");
+// }
+
+//ta bort 
+//listaNamn.Remove("");
+
+Console.Write("Ange ett nytt namn till listan: ");
+listaNamn.Add(Console.ReadLine()!);
+
+Console.WriteLine($"abc: {string.Join(", ", listaNamn)}");
---
+++ kap5/Mileage/Program.cs
@@ -0,0 +1,26 @@
+﻿List<string> listaMat = ["Curry", "Pierogi", "Rice"];
+Console.WriteLine($"Matlista:");
+foreach (var mat in listaMat)
+{
+    Console.Write($" {mat}, ");
+}
+
+Console.WriteLine();
+
+List<int> listaÅrtal = [996, 1918, 966, 1945, 1992];
+Console.Write("Årtal: ");
+foreach (var år in listaÅrtal)
+{
+    Console.Write($"{år}, ");
+}
+
+Console.WriteLine();
+
+List<string> listaNamn = [];
+
+Console.WriteLine("Lägg till namn en namnlista");
+while (true)
+{
+    Console.Write("Ange ett namn att lägga till: ");
+    listaNamn.Add(Console.ReadLine()!);
+}
